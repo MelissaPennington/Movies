a99c513c33d6bf2f28a637291eb9044bca1c137c
melissapennington <mlpennington9@gmail.com>
2024-12-20 08:11:33 -0600

    tweak the layout

--- Movies/src/components/MovieCard.jsx
+++ Movies/src/components/MovieCard.jsx
@@ -1,13 +1,15 @@
+import React from "react";
+import "./MovieCard.css"; // Custom CSS for styling
 
-function MovieCard({movie}) {
+function MovieCard({ movie }) {
+  function onFavoriteClick() {
+    alert("Favorite clicked");
+  }
 
-    function onFavoriteClick() {
-        alert("clicked")
-    }
-
-    return <div className="movie-card">
+  return (
+    <div className="movie-card">
       <div className="movie-poster">
-        <img src={movie.url} alt={movie.title}/>
+        <img src={movie.url} alt={movie.title} />
         <div className="movie-overlay">
           <button className="favorite-btn" onClick={onFavoriteClick}>
             🤍
@@ -18,7 +20,33 @@ function MovieCard({movie}) {
         <h3>{movie.title}</h3>
         <p>{movie.release_date}</p>
       </div>
-  </div>
-} 
+    </div>
+  );
+}
 
-export default MovieCard
+export default MovieCard;
+
+
+// function MovieCard({movie}) {
+
+//     function onFavoriteClick() {
+//         alert("clicked")
+//     }
+
+//     return <div className="movie-card">
+//       <div className="movie-poster">
+//         <img src={movie.url} alt={movie.title}/>
+//         <div className="movie-overlay">
+//           <button className="favorite-btn" onClick={onFavoriteClick}>
+//             🤍
+//           </button>
+//         </div>
+//       </div>
+//       <div className="movie-info">
+//         <h3>{movie.title}</h3>
+//         <p>{movie.release_date}</p>
+//       </div>
+//   </div>
+// } 
+
+// export default MovieCard
